4a11b873c680616007e4b279c3c2decaf104279b
ncorreia47 <dev.ncorr@gmail.com>
2026-02-09 16:46:21 -0300

    feat: add landing to bronze dag

---
+++ dags/dag_landing_to_bronze.py
@@ -0,0 +1,32 @@
+from airflow.sdk import dag, task, task_group
+from datetime import datetime
+
+DBT_PROJECT_DIR = "/opt/airflow/dbt/elt_data_pipeline"
+
+@dag(
+    dag_id="dag_landing_to_bronze",
+    start_date=datetime(2025, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags = ['data-engineering', 'dbt', 'bronze']
+)
+
+
+def landing_to_bronze():
+
+    @task.bash(cwd=DBT_PROJECT_DIR)
+    def setup_dbt():
+        return "dbt deps"
+
+    @task_group(group_id="bronze_layer")
+    def bronze_layer():
+        
+        @task.bash(cwd=DBT_PROJECT_DIR)
+        def groups():
+            return "dbt run --models bronze.groups"
+        
+        groups()
+
+    setup_dbt() >> bronze_layer()
+
+landing_to_bronze()
