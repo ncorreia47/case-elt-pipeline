ab5603637efb774ae582c01ef795f7f3c6781846
ncorreia47 <dev.ncorr@gmail.com>
2026-02-10 18:46:36 -0300

    feat: add sla_response_limit field

--- dbt/elt_data_pipeline/models/silver/ticket_sla_events_silver.sql
+++ dbt/elt_data_pipeline/models/silver/ticket_sla_events_silver.sql
@@ -14,15 +14,16 @@ with ticket_sla_events_bronze as (
 , final as (
 
     select
-        id                      as cd_ticket_sla_event_id
-      , sla_policy_id           as cd_sla_policy_id
-      , upper(sla_policy_title) as ds_sla_policy_title
-      , time                    as dt_time
-      , upper(type)             as ds_type
-      , upper(metric)           as ds_metric
-      , ticket_id               as cd_ticket_id
-      , current_timestamp       as ingested_at
-      , {{ snapshot_ts() }}     as snapshot_ts
+        id                                              as cd_ticket_sla_event_id
+      , sla_policy_id                                   as cd_sla_policy_id
+      , upper(sla_policy_title)                         as ds_sla_policy_title
+      , regexp_replace(sla_policy_title, '\D', '', 'g') as nr_sla_response_limit_in_hour
+      , time                                            as dt_time
+      , upper(type)                                     as ds_type
+      , upper(metric)                                   as ds_metric
+      , ticket_id                                       as cd_ticket_id
+      , current_timestamp                               as ingested_at
+      , {{ snapshot_ts() }}                             as snapshot_ts
     from ticket_sla_events_bronze
 	
 )
