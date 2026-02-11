7e3b311bbd982d57e27f796aabf00641bdce074a
ncorreia47 <dev.ncorr@gmail.com>
2026-02-11 09:18:40 -0300

    feat: add start_time param to incremental strategy

--- dags/dag_api_get_data.py
+++ dags/dag_api_get_data.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from airflow.sdk import dag, task, task_group
+from airflow.models.param import Param
 from datetime import datetime
 from api.api_get_data import api_get_data
 from api.enum_endpoints import Endpoints
@@ -10,14 +11,18 @@ from api.enum_endpoints import Endpoints
     schedule = None,
     start_date = datetime(2025, 1, 1),
     catchup = False,
+    params={
+        "start_time": Param(
+            default=None,
+            type=["string", "null"],
+            description="Informe uma data no padrao yyyy-mm-dd para definir uma data de corte na extracao dos dados da API"
+        )
+    },
     tags = ['data-engineering', 'api', 'ingest']
 )
 
 
 def create_api_tasks_dag():
-
-    # Parametro dinâmico para cargas incrementais e reprocessamentos
-    #start_time_param = "{{ dag_run.conf['start_time'] if dag_run.conf and dag_run.conf.get('start_time') else None }}"
 
     @task_group(group_id="get_data_group")
     def get_data_group():
@@ -29,9 +34,13 @@ def create_api_tasks_dag():
             task_id = f"get_data_{endpoint.value}"
 
             @task(task_id=task_id)
-            def get_data(endpoint: Endpoints):
+            def get_data(endpoint: Endpoints, params=None):
                 print(f"Iniciando a coleta de dados do endpoint {endpoint.value}")
-                api_get_data(endpoint)
+                
+                start_time_value = params["start_time"]
+                start_time_value = datetime.strptime(start_time_value, '%Y-%m-%d').isoformat() if start_time_value else None
+
+                api_get_data(endpoint, start_time_value)
                 return f"Dados coletados para o endpoint {endpoint.value} com sucesso!"
 
             get_data(endpoint)
